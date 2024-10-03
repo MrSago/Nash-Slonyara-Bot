9fe868aefbf743ada57dc87397f9b5e0ef3e5b35
MrSago <37180898+MrSago@users.noreply.github.com>
2024-10-03 09:29:13 +0300

    (●'◡'●)

--- index.js
+++ index.js
@@ -24,62 +24,73 @@ bot.onText(`^\/start(@${config.telegram.login})?$`, (msg) => {
 });
 
 bot.onText(`^\/vpn(@${config.telegram.login})?$`, async (msg) => {
-  const chat_id = msg.chat.id;
-  if (msg.chat.type !== "private") {
-    bot.sendMessage(
-      chat_id,
-      "Эта команда доступна только в личных сообщениях бота!"
-    );
-    return;
-  }
+  try {
+    const chat_id = msg.chat.id;
+    if (msg.chat.type !== "private") {
+      console.log(char_id);
 
-  const user_id = msg.from.id;
-  const member = await bot.getChatMember(config.telegram.group_id, user_id);
-  if (member.status !== "administrator" && member.status !== "creator") {
-    bot.sendMessage(
-      chat_id,
-      "Эта команда доступна только участникам приватной группы!"
-    );
-    return;
-  }
-
-  await bot.sendMessage(chat_id, "Ваш файлик подготавливается, ожидайте...");
-
-  const user_login = msg.from.username;
-  const tmp_input_file = `${os.tmpdir()}/${user_login}`;
-
-  fs.writeFileSync(tmp_input_file, `1\n${user_login}\n1\n`, "utf8");
-
-  const input = fs.createReadStream(tmp_input_file);
-  const child = spawn("/root/openvpn-config.sh");
-  input.pipe(child.stdin);
-
-  child.stdout.pipe(process.stdout);
-  child.stderr.pipe(process.stderr);
-
-  child.on("close", (code) => {
-    logger.info(`child process exited with code ${code}`);
-
-    fs.unlinkSync(tmp_input_file);
-
-    const ovpn_file = `/root/${user_login}.ovpn`;
-    const stream = fs.createReadStream(ovpn_file);
-    bot
-      .sendDocument(
+      bot.sendMessage(
         chat_id,
-        stream,
-        {},
-        {
-          contentType: "application/octet-stream",
-        }
-      )
-      .then(() => {
-        fs.unlinkSync(ovpn_file);
-      })
-      .catch((err) => {
-        logger.error(err);
-      });
-  });
+        "Эта команда доступна только в личных сообщениях бота!"
+      );
+      return;
+    }
+
+    const user_id = msg.from.id;
+    const member = await bot.getChatMember(config.telegram.group_id, user_id);
+    if (member.status !== "administrator" && member.status !== "creator") {
+      console.log(chat_id);
+
+      bot.sendMessage(
+        chat_id,
+        "Эта команда доступна только участникам приватной группы!"
+      );
+      return;
+    }
+
+    console.log(chat_id);
+    await bot.sendMessage(chat_id, "Ваш файлик подготавливается, ожидайте...");
+
+    const user_login = msg.from.username;
+    const tmp_input_file = `${os.tmpdir()}/${user_login}`;
+
+    fs.writeFileSync(tmp_input_file, `1\n${user_login}\n1\n`, "utf8");
+
+    const input = fs.createReadStream(tmp_input_file);
+    const child = spawn("/root/openvpn-config.sh");
+    input.pipe(child.stdin);
+
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+
+    child.on("close", (code) => {
+      logger.info(`child process exited with code ${code}`);
+
+      fs.unlinkSync(tmp_input_file);
+
+      const ovpn_file = `/root/${user_login}.ovpn`;
+      const stream = fs.createReadStream(ovpn_file);
+      console.log(chat_id);
+
+      bot
+        .sendDocument(
+          chat_id,
+          stream,
+          {},
+          {
+            contentType: "application/octet-stream",
+          }
+        )
+        .then(() => {
+          fs.unlinkSync(ovpn_file);
+        })
+        .catch((err) => {
+          logger.error(err);
+        });
+    });
+  } catch (err) {
+    logger.error(err);
+  }
 });
 
 logger.info("Nash-Slonyara-Bot started!");
