871afc9388491a6e63e3d0c7b9ef4b3472a94c27
MrSago <37180898+MrSago@users.noreply.github.com>
2024-10-04 03:15:41 +0300

    upd

--- help_messages.js
+++ help_messages.js
@@ -8,11 +8,12 @@ const startMessage =
 
 const helpMessage =
   "*Инструкция по установке и настройке OpenVPN*\n\n" +
-  "1.   Получите OpenVPN-ключ (.ovpn) с помощью команды /ovpn в личном чате боте.\n" +
+  "1. Получите OpenVPN-ключ (.ovpn) с помощью команды /ovpn в личном чате боте.\n" +
   "2.1. Установите и настройте OpenVPN согласно [инструкции](https://wiki.aeza.net/openvpn-sozdanie-lichnoi-virtualnoi-chastnoi-seti#id-3.-skachivanie-i-podklyuchenie-klienta-openvpn-na-pk) (п.3).\n" +
-  "2.2. Настройка для Android и iOS выглядит аналогично\n" +
-  "3.   Пользуемся!\n\n" +
-  "OpenVPN для [Windows](https://openvpn.net/community-downloads/) и [Android](https://play.google.com/store/apps/details?id=net.openvpn.openvpn&hl=ru) версии." +
+  "2.2. Настройка для Android и iOS выглядит аналогично.\n" +
+  "3. Пользуемся!\n\n" +
+  "OpenVPN для [Windows](https://openvpn.net/community-downloads/) и [Android](https://play.google.com/store/apps/details?id=net.openvpn.openvpn&hl=ru) версии.\n" +
+  "Рекомендуемые настройки Android для более стабильной работы /android.\n" +
   "По всем вопросам обращайтесь в наш чатик.";
 
 const androidMessage = "Рекомендуемые настройки для Android: (soon)";
