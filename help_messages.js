5753e442d488fe5c1047eeb703d782c71134a0d0
MrSago <37180898+MrSago@users.noreply.github.com>
2024-10-04 03:09:04 +0300

    fix

--- help_messages.js
+++ help_messages.js
@@ -7,16 +7,11 @@ const startMessage =
   "/ovpn - получение OpenVPN-ключа";
 
 const helpMessage =
-  "**Инструкция по установке и настройке OpenVPN**\n\n" +
-  "1. Получите OpenVPN-ключ (\*.ovpn) с помощью команды /ovpn в личном чате боте.\n" +
-  "2. Установите и настройте OpenVPN согласно [инструкции](https://wiki.aeza.net/openvpn-sozdanie-lichnoi-virtualnoi-chastnoi-seti#id-3.-skachivanie-i-podklyuchenie-klienta-openvpn-na-pk) (пункт 3).\n" +
+  "*Инструкция по установке и настройке OpenVPN*\n\n" +
+  "1. Получите OpenVPN-ключ (.ovpn) с помощью команды /ovpn в личном чате боте.\n" +
+  "2. Установите и настройте OpenVPN согласно [инструкции](https://wiki.aeza.net/openvpn-sozdanie-lichnoi-virtualnoi-chastnoi-seti#id-3.-skachivanie-i-podklyuchenie-klienta-openvpn-na-pk) (п.3).\n" +
   "3. Пользуемся!\n\n" +
   "OpenVPN для [Windows](https://openvpn.net/community-downloads/) и [Android](https://play.google.com/store/apps/details?id=net.openvpn.openvpn&hl=ru) версии.";
-// "Инструкция по установке и настройке OpenVPN: " +
-// "[здесь](https://wiki.aeza.net/openvpn-sozdanie-lichnoi-virtualnoi-chastnoi-seti#id-3.-skachivanie-i-podklyuchenie-klienta-openvpn-na-pk)\n" +
-// "Аналогично делается для мобильных устройств.\n" +
-// "OpenVPN для [Windows](https://openvpn.net/community-downloads/) " +
-// "и [Android](https://play.google.com/store/apps/details?id=net.openvpn.openvpn&hl=ru) версии.";
 
 const androidMessage = "Рекомендуемые настройки для Android: (soon)";
 
