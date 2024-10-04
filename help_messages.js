bebf6096c016a7ad537913e3ba94966722708074
MrSago <37180898+MrSago@users.noreply.github.com>
2024-10-04 03:12:48 +0300

    upd help message

--- help_messages.js
+++ help_messages.js
@@ -8,10 +8,12 @@ const startMessage =
 
 const helpMessage =
   "*Инструкция по установке и настройке OpenVPN*\n\n" +
-  "1. Получите OpenVPN-ключ (.ovpn) с помощью команды /ovpn в личном чате боте.\n" +
-  "2. Установите и настройте OpenVPN согласно [инструкции](https://wiki.aeza.net/openvpn-sozdanie-lichnoi-virtualnoi-chastnoi-seti#id-3.-skachivanie-i-podklyuchenie-klienta-openvpn-na-pk) (п.3).\n" +
-  "3. Пользуемся!\n\n" +
-  "OpenVPN для [Windows](https://openvpn.net/community-downloads/) и [Android](https://play.google.com/store/apps/details?id=net.openvpn.openvpn&hl=ru) версии.";
+  "1.   Получите OpenVPN-ключ (.ovpn) с помощью команды /ovpn в личном чате боте.\n" +
+  "2.1. Установите и настройте OpenVPN согласно [инструкции](https://wiki.aeza.net/openvpn-sozdanie-lichnoi-virtualnoi-chastnoi-seti#id-3.-skachivanie-i-podklyuchenie-klienta-openvpn-na-pk) (п.3).\n" +
+  "2.2. Настройка для Android и iOS выглядит аналогично\n" +
+  "3.   Пользуемся!\n\n" +
+  "OpenVPN для [Windows](https://openvpn.net/community-downloads/) и [Android](https://play.google.com/store/apps/details?id=net.openvpn.openvpn&hl=ru) версии." +
+  "По всем вопросам обращайтесь в наш чатик.";
 
 const androidMessage = "Рекомендуемые настройки для Android: (soon)";
 
